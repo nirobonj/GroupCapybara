decbc3526136ffaa418eb232826bea914c8a823b
aunlaphat <aunlaphat5@hotmail.com>
2024-09-05 22:03:07 +0700

    commit version

--- nail_project/bootstrap/app.php
+++ nail_project/bootstrap/app.php
@@ -1,9 +1,12 @@
 <?php
+require __DIR__.'/vendor/autoload.php';
 
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+// Testing if the Application class is correctly loaded
+$app = new Application();
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
