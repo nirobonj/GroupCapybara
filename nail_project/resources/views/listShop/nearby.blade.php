f30ba28f8ed0121b3f62557f65b0d6927cb76b4e
KhunSanprom <khun.sanprom@gmail.com>
2024-09-12 18:36:17 +0700

    add layout + addlistShop.css +  update listShopView
    - add layout-> navbar_frame
    - create listShop.css
    - update listShopView

--- nail_project/resources/views/listShop/nearby.blade.php
+++ nail_project/resources/views/listShop/nearby.blade.php
@@ -2,15 +2,19 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ร้านทำเล็บใกล้ฉัน</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>NailSlay</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="{{ asset('css/listShop.css') }}">
 </head>
-<body>
+<body class="font-sans antialiased dark:bg-black dark:text-white/50 background-color">
+    @include('layouts.navbar_frame')
     <div class="container">
-        <h1>ร้านทำเล็บใกล้ฉัน</h1>
-        <p>นี่คือรายชื่อร้านทำเล็บใกล้คุณ:</p>
         <!-- ใส่เนื้อหาที่ต้องการแสดง -->
     </div>
 </body>
