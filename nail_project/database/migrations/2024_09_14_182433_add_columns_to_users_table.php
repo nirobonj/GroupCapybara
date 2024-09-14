405c7d60e59093658006e6720fd81c3f842e72fa
KanokpornM <kanokporn6760@gmail.com>
2024-09-15 01:32:32 +0700

    แก้ index ในตาราง user

---
+++ nail_project/database/migrations/2024_09_14_182433_add_columns_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+{
+    Schema::table('users', function (Blueprint $table) {
+        // เพิ่มคอลัมน์ใหม่
+        $table->string('phone_number')->nullable();
+        $table->integer('province_id')->nullable();
+        $table->integer('district_id')->nullable();
+        $table->boolean('user_type')->nullable();
+
+        // เพิ่ม index ให้กับคอลัมน์ province_id และ district_id
+        $table->index('province_id');
+        $table->index('district_id');
+    });
+}
+
+public function down()
+{
+    Schema::table('users', function (Blueprint $table) {
+        // ลบคอลัมน์ใหม่
+        $table->dropColumn(['phone_number', 'province_id', 'district_id','user_type']);
+
+        // ลบ index ที่สร้างไว้
+        $table->dropIndex(['province_id']);
+        $table->dropIndex(['district_id']);
+    });
+}
+};
