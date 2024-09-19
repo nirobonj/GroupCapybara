b5432e2dc135c73f231bf7f880357faa140b874e
KanokpornM <kanokporn6760@gmail.com>
2024-09-20 00:49:26 +0700

    update path log in

--- nail_project/routes/web.php
+++ nail_project/routes/web.php
@@ -21,8 +21,8 @@ Route::get('register', [RegisterController::class, 'showRegistrationForm'])->nam
 Route::post('register', [RegisterController::class, 'register'])->name('register');
 Route::get('get-districts/{provinceId}', [RegisterController::class, 'getDistricts']);
 
-Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('login', [LoginController::class, 'login']);
+Route::get('/', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/', [LoginController::class, 'login']);
 
 Route::middleware(['auth', 'is_admin'])->group(function () {
     Route::get('/mbooking', [SalonReservationController::class, 'index'])->name('mbooking');
